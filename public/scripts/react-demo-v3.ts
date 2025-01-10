ec51bf8583eb55b73389864eabb3ca6ca0d67963
betteree <kkne1017@gmail.com>
2025-01-10 09:33:48 +0900

    [정리] React 컴포넌트 호출 방법

--- public/scripts/react-demo-v3.ts
+++ public/scripts/react-demo-v3.ts
@@ -4,19 +4,7 @@
 import React from '../lib/react.js';
 import ReactDOM from '../lib/react-dom/client.js';
 
-// React 엘리먼트
 const heading = React.createElement('h1', {}, '안녕!');
-
-// React 컴포너트(이름 작성 규칙 : PascalCase)
-function HeadingOne() {
-  // React 엘리먼트 (생성 후) 반환
-  return React.createElement('h1', { children: '안녕!' });
-}
-
-for (let i = 0; i < 5; ++i) {
-  // 컴포넌트를 사용해 React 엘리먼트 재사용할 수 있음!
-  console.log(React.createElement(HeadingOne));
-}
 
 const changeButton = React.createElement(
   'button',
@@ -35,3 +23,42 @@ const rootElement = document.getElementById('react');
 const reactDomRoot = ReactDOM.createRoot(rootElement);
 
 reactDomRoot.render(wrapper);
+
+// 코드(React 엘리먼트)를 재사용하자
+// let a = 1;
+// let b = 10;
+// let c = a + b;
+
+{
+  // a
+  const heading = React.createElement('h1', {}, '안녕!');
+
+  // b
+  const changeButton = React.createElement(
+    'button',
+    { type: 'button' },
+    '인사말'
+  );
+
+  // c
+  const wrapper = React.createElement(
+    React.Fragment,
+    null,
+    heading,
+    changeButton
+  );
+}
+
+// 재사용 메커니즘 : 클래스(x) 또는 함수(v)
+// 리액트 월드 재사용 메커니즘 : 컴포넌트(함수)
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(1, 10));
+
+// a = 11;
+// b = 210;
+// c = a + b;
+
+console.log(add(11, 210));
